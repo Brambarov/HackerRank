de0121f993a35dee966e7af74fac64aa848160eb
Brambarov <ivan.brambarov@gmail.com>
2024-09-15 15:06:23 +0300

    HackerRankProblems2

---
+++ HackerRankProblems2/HackerRankProblems2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace HackerRankProblems2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Result.LonelyInteger(a);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackerRankProblems2/HackerRankProblems2/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems2
+{
+    public class Result
+    {
+        public static int LonelyInteger (List<int> a)
+        {
+            int result = 0;
+            int firstNumber = 0;
+            int secondNumber = 0;
+
+            a.Sort();
+
+            for (int i = 0; i< a.Count; i = i + 2)
+            {
+                firstNumber = a[i];
+
+                if (i == (a.Count - 1))
+                {
+                    result = firstNumber;
+                    break;
+                }
+
+                secondNumber = a[i + 1];
+
+                if (firstNumber != secondNumber)
+                {
+                    result = firstNumber;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
