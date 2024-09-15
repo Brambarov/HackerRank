b88320fe94bac5582676d06e061545c81791b483
Brambarov <ivan.brambarov@gmail.com>
2024-09-15 21:58:46 +0300

    Add mock test for problems 2

--- HackerRankProblems2/HackerRankProblems2/Program.cs
+++ HackerRankProblems2/HackerRankProblems2/Program.cs
@@ -6,13 +6,42 @@ namespace HackerRankProblems2
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            //int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+            //List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
 
-            int result = Result.LonelyInteger(a);
+            //int result = Result.LonelyInteger(a);
 
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
+
+
+
+            //int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            //List<int> result = Result.CountingSort(arr);
+
+            //Console.WriteLine(String.Join(" ", result));
+
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<List<int>> matrix = new List<List<int>>();
+
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    matrix.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+                }
+
+                int result = Result.FlippingMatrix(matrix);
+
+                Console.WriteLine(result);
+            }
         }
     }
 }
--- HackerRankProblems2/HackerRankProblems2/Result.cs
+++ HackerRankProblems2/HackerRankProblems2/Result.cs
@@ -8,7 +8,7 @@ namespace HackerRankProblems2
 {
     public class Result
     {
-        public static int LonelyInteger (List<int> a)
+        public static int LonelyInteger(List<int> a)
         {
             int result = 0;
             int firstNumber = 0;
@@ -16,7 +16,7 @@ namespace HackerRankProblems2
 
             a.Sort();
 
-            for (int i = 0; i< a.Count; i = i + 2)
+            for (int i = 0; i < a.Count; i = i + 2)
             {
                 firstNumber = a[i];
 
@@ -37,5 +37,95 @@ namespace HackerRankProblems2
 
             return result;
         }
+
+        public static List<int> CountingSort(List<int> arr)
+        {
+            List<int> frequencyArray = new List<int>(new int[100]);
+            int value = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                value = arr[i];
+                frequencyArray[value]++;
+            }
+
+
+            return frequencyArray;
+        }
+
+        public static int FlippingMatrix(List<List<int>> matrix)
+        {
+            List<int> row = new List<int>();
+            List<int> column = new List<int>();
+            int number = 0;
+            int mirrorNumber = 0;
+            int result = 0;
+
+            for (int j = 0; j < matrix.Count; j++)
+            {
+                for (int i = 0; i < matrix.Count; i++)
+                {
+                    column.Add(matrix[i][j]);
+                }
+
+                for (int k = 0; k < column.Count / 2; k++)
+                {
+                    number = column[k];
+                    mirrorNumber = column[column.Count - 1 - k];
+
+                    if (number < mirrorNumber)
+                    {
+                        column[k] = mirrorNumber;
+                        column[column.Count - 1 - k] = number;
+                    }
+                }
+
+                for (int i = 0; i < column.Count; i++)
+                {
+                    matrix[i][j] = column[i];
+                }
+
+                number = 0;
+                mirrorNumber = 0;
+                column.Clear();
+            }
+
+            for (int i = 0; i < matrix.Count / 2; i++)
+            {
+                row = matrix[i];
+
+                for (int j = 0; j < row.Count / 2; j++)
+                {
+                    number = row[j];
+                    mirrorNumber = row[row.Count - 1 - j];
+
+                    if (number < mirrorNumber)
+                    {
+                        row[j] = mirrorNumber;
+                        row[row.Count - 1 - j] = number;
+                    }
+                }
+
+                number = 0;
+                mirrorNumber = 0;
+            }
+
+            for (int i = 0; i < matrix.Count / 2; i++)
+            {
+                for (int j = 0; j < matrix.Count / 2; j++)
+                {
+                    result += matrix[i][j];
+                }
+            }
+
+            return result;
+        }
     }
 }
+
+//1
+//2
+//112 42 83 119
+//56 125 56 49
+//15 78 101 43
+//62 98 114 108
