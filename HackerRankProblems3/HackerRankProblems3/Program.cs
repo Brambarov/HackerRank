d7afa7961bfc896a2da713d5ca2f2c8bc370731e
Brambarov <ivan.brambarov@gmail.com>
2024-09-18 01:38:06 +0300

    HackerRankProblems3

---
+++ HackerRankProblems3/HackerRankProblems3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+
+namespace HackerRankProblems3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int t = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < t; i++)
+            //{
+            //    int n = Convert.ToInt32(Console.ReadLine());
+
+            //    List<int> arr = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+
+            //    List<int> result = Result.FindZigZagSequence(arr);
+
+            //    Console.WriteLine(String.Join(' ', result));
+            //}
+
+
+
+            //int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //for (int tItr = 0; tItr < t; tItr++)
+            //{
+            //    string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            //    int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            //    int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            //    int result = Result.TowerBreakers(n, m);
+
+            //    Console.WriteLine(result);
+            //}
+
+
+
+            //int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //string s = Console.ReadLine();
+
+            //int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //string result = Result.CaesarCipher(s, k);
+
+            //Console.WriteLine(result);
+
+
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = Result.PalindromeIndex(s);
+
+                Console.WriteLine(result);
+            }
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HackerRankProblems3/HackerRankProblems3/Result.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems3
+{
+    public class Result
+    {
+        public static List<int> FindZigZagSequence(List<int> arr)
+        {
+            List<int> result = new List<int>(arr);
+            int firstValue = 0;
+            int lastValue = 0;
+            int j = result.Count - 1;
+
+            result.Sort();
+
+            for (int i = result.Count / 2; i < result.Count / 2 + result.Count / 4 + 1; i++)
+            {
+                firstValue = result[i];
+                lastValue = result[j];
+
+                result[i] = lastValue;
+                result[j] = firstValue;
+
+                j--;
+            }
+
+            return result;
+        }
+
+        public static int TowerBreakers(int n, int m)
+        {
+            int result = m == 1 ? 2 : n % 2 == 1 ? 1 : 2;
+
+            return result;
+
+            if (m == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                if (n % 2 == 1)
+                {
+                    return 1;
+                }
+
+                return 2;
+            }
+        }
+
+        public static string CaesarCipher(string s, int k)
+        {
+            char[] charArray = s.ToCharArray();
+            string result = string.Empty;
+            k %= 26;
+
+            for (int i = 0; i < s.Count(); i++)
+            {
+                char c = charArray[i];
+
+                int asciiCode = Convert.ToInt32(c);
+
+                if (65 <= asciiCode && asciiCode <= 90)
+                {
+                    if (asciiCode + k > 90)
+                    {
+                        asciiCode += (k - 26);
+                    }
+                    else
+                    {
+                        asciiCode += k;
+                    }
+                }
+                else if (97 <= asciiCode && asciiCode <= 122)
+                {
+                    if (asciiCode + k > 122)
+                    {
+                        asciiCode += (k - 26);
+                    }
+                    else
+                    {
+                        asciiCode += k;
+                    }
+                }
+
+                c = Convert.ToChar(asciiCode);
+
+                result = result + c;
+            }
+
+            return result;
+        }
+
+        public static int PalindromeIndex(string s)
+        {
+            int index = -1;
+            int j = s.Length - 1;
+            string first = string.Empty;
+            string second = string.Empty;
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                first = s.Substring(i, 1);
+                second = s.Substring(j, 1);
+
+                if (!first.Equals(second))
+                {
+                    if (first.Equals(s.Substring(j - 1, 1)))
+                    {
+                        if (index != - 1)
+                        {
+                            return -1;
+                        }
+
+                        index = j;
+                        j--;
+                    }
+                    else if (second.Equals(s.Substring(i + 1, 1)))
+                    {
+                        if (index != -1)
+                        {
+                            return -1;
+                        }
+
+                        index = i;
+                        i++;
+                    }
+                }
+
+                j--;
+            }
+
+            return index;
+        }
+    }
+}
